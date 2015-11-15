92d8d440043cc0a801e6e8df2149aea292b7b755
project_hephaestus <antwnianas@gmail.com>
2015-11-15 12:46:49 +0200

    code

---
+++ gridlabd and database/gridlab-d-code-5225-trunk/core/build.h
@@ -0,0 +1,1 @@
+#define BUILDNUM Last Changed Rev: = 
---
+++ gridlabd and database/gridlab-d-code-5225-trunk/core/config_external.h
@@ -0,0 +1,28 @@
+// config.h automatically created ’¨  18/11/2014 12:55:42,21 
+// ..\utilities\configure: config=config_external.h 
+// ..\utilities\configure: search=C:\ 
+// ..\utilities\configure: source=.. 
+// : MYSQL target include\mysql.h not found  
+// 
+// MATLAB x64 configuration 
+// 
+// ..\utilities\configure: found C:\PROGRA~1\MATLAB\R2011a\extern\include\mex.h 
+#if defined _M_X64 || defined _M_IA64 
+#define MATLAB_X64 
+#endif 
+ 
+#ifdef MATLAB_X64 
+#define MATLAB_INCLUDE "C:\PROGRA~1\MATLAB\R2011a\extern\include" 
+#define MATLAB_LIB "C:\PROGRA~1\MATLAB\R2011a\extern\lib\win64\MICROS~1" 
+#define HAVE_MATLAB "C:\PROGRA~1\MATLAB\R2011a" 
+#endif 
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: PowerWorld SimAuto target RegSimAuto.exe not found  
